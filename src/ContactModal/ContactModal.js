0be539f90b6883b18d502d173ff51b44e0eef384
antoniotex <acm.skt@gmail.com>
2022-02-09 00:34:45 -0300

    Ensure ContactModal submit button able if form is valid

--- src/ContactModal/ContactModal.js
+++ src/ContactModal/ContactModal.js
@@ -1,10 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 
 export const ContactModal = () => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [email, setEmail] = useState('');
+  const [isValid, setIsValid] = useState(false);
+
+  useEffect(() => {
+    setIsValid(
+      !!name &&
+        !!phone &&
+        !!email &&
+        /^[0-9]{5}-[0-9]{4}$/.test(phone) &&
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email),
+    );
+  }, [name, phone, email]);
 
   return (
     <div className={styles.main}>
@@ -23,7 +34,7 @@ export const ContactModal = () => {
           value={email}
         />
 
-        <button>Submit</button>
+        <button disabled={!isValid}>Submit</button>
       </form>
     </div>
   );
--- src/ContactModal/ContactModal.test.js
+++ src/ContactModal/ContactModal.test.js
@@ -1,4 +1,4 @@
-import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 import { ContactModal } from './';
 
 describe('ContactModal', () => {
@@ -9,6 +9,7 @@ describe('ContactModal', () => {
     const nameInput = screen.queryByPlaceholderText('Name');
     const phoneInput = screen.queryByPlaceholderText('Phone Number');
     const emailInput = screen.queryByPlaceholderText('Email Address');
+    const submitButton = screen.getByText('Submit');
 
     expect(nameInput).toBeInTheDocument();
     expect(phoneInput).toBeInTheDocument();
@@ -17,5 +18,21 @@ describe('ContactModal', () => {
     expect(nameInput).toHaveValue('');
     expect(phoneInput).toHaveValue('');
     expect(emailInput).toHaveValue('');
+
+    expect(submitButton).toBeDisabled();
+  });
+
+  test('Disables submit button until form is valid', () => {
+    render(<ContactModal />);
+    const nameInput = screen.queryByPlaceholderText('Name');
+    const phoneInput = screen.queryByPlaceholderText('Phone Number');
+    const emailInput = screen.queryByPlaceholderText('Email Address');
+    const submitButton = screen.getByText('Submit');
+
+    fireEvent.change(nameInput, { target: { value: 'Port Exe' } });
+    fireEvent.change(phoneInput, { target: { value: '99679-6217' } });
+    fireEvent.change(emailInput, { target: { value: 'port@exe.com' } });
+
+    expect(submitButton).not.toBeDisabled();
   });
 });
